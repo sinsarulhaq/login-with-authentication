5bb2826b664c62ee546bec2dee782698c6cb1b58
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2021-10-25 12:53:43 +0530

    Basics of user routers almost finished

--- models/user.js
+++ models/user.js
@@ -4,57 +4,57 @@ const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name: {
-        type:String,
-        trim:true,
-        required:true
-    },email: {
-        unique:true,
-        lowercase:true,
-        type:String,
-        trim:true,
-        required:true,
-        validate(value){
-            if(!validator.isEmail(value)){
+        type: String,
+        trim: true,
+        required: true
+    }, email: {
+        unique: true,
+        lowercase: true,
+        type: String,
+        trim: true,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error('please provide a valid email')
             }
         }
-    },password: {
-        type:String,
-        minlength:7,
-        maxlength:10,
-        required:true,
-        validate(value){
-            if(!value.includes(value)){
-                throw new Error('please contain a password') 
+    }, password: {
+        type: String,
+        minlength: 7,
+        maxlength: 10,
+        required: true,
+        validate(value) {
+            if (!value.includes(value)) {
+                throw new Error('please contain a password')
             }
         }
-    },age:{
-        type:Number,
-        default:0,
-        validate(value){
-            if(value < 0){
+    }, age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
                 throw new Error('please contain a password')
             }
         }
     }
 })
 
-userSchema.statics.findByChecking = async (email,password)=>{
-    const user =await User.findOne({email})
-    if(!email){
+userSchema.statics.findByChecking = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!email) {
         throw new Error('Unable to Login!')
     }
-    const isMatch = await bcrypt.compare(password,user.password)
-    if(!isMatch){
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
         throw new Error('Unable to Login!')
     }
     return user
 }
 
-userSchema.pre('save', async function(){
+userSchema.pre('save', async function () {
     const user = this
-    if(user.isModified('password')){
-        user.password = await bcrypt.hash(user.password,8)
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
     }
 })
 
--- router/userR.js
+++ router/userR.js
@@ -13,6 +13,7 @@ router.post('/users',async(req, res)=>{
         res.status(400).send(e)
     }
 })
+
 router.post('/users/login',async(req, res)=>{
     try{
         const user = await User.findByChecking(req.body.email,req.body.password)
@@ -21,4 +22,42 @@ router.post('/users/login',async(req, res)=>{
         res.status(404).send(e)
     }
 })
+
+router.get('/users/:id', async(req, res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user){
+           return res.status(404).send()
+        }
+        res.status(201).send(user) 
+    }catch(e){
+        res.status(404).send(e)
+    }
+})
+
+router.patch('/users/update/:id', async(req, res)=>{
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id,req.body)
+        if(!user){
+           return res.status(404).send()
+        }
+         res.status(201).send(user)
+    }catch(e){
+        res.status(404).send(user)
+    }
+})
+
+router.delete('/users/delete/:id', async(req, res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user){
+           return res.status(404).send()
+        }
+        res.status(201).send(user)
+    }catch(e){
+        res.status(404).send(e)
+    }
+})
+
+
 module.exports = router
