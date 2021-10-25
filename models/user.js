e295b344483deafd9e7b175accae5f45bde54a44
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2021-10-25 10:36:01 +0530

    create user and login user sections finished using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -38,6 +39,24 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByChecking = async (email,password)=>{
+    const user =await User.findOne({email})
+    if(!email){
+        throw new Error('Unable to Login!')
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        throw new Error('Unable to Login!')
+    }
+    return user
+}
+
+userSchema.pre('save', async function(){
+    const user = this
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password,8)
+    }
+})
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
--- router/userR.js
+++ router/userR.js
@@ -4,6 +4,21 @@ const router = new express.Router()
 
 
 //routers
-
-
+router.post('/users',async(req, res)=>{
+    try{
+        const user = await User(req.body)
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+router.post('/users/login',async(req, res)=>{
+    try{
+        const user = await User.findByChecking(req.body.email,req.body.password)
+        res.status(200).send(user)
+    }catch(e){
+        res.status(404).send(e)
+    }
+})
 module.exports = router
